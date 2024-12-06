f4a0a384feacd131ba07d58af6f3329a27e49a83
SM <jshgonzalez919@gmail.com>
2024-12-05 16:52:11 -0800

    initial commit

---
+++ src/main/java/dev/J/RepositoryForFamilies/RepositoryForFamiliesApplication.java
@@ -0,0 +1,13 @@
+package dev.J.RepositoryForFamilies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryForFamiliesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryForFamiliesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/J/RepositoryForFamilies/RepositoryForFamiliesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.J.RepositoryForFamilies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryForFamiliesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
