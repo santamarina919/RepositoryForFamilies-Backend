26a1edcff06b61afbc3abb8c0046173624758aab
SM <jsantamarina919@gmail.com>
2025-04-24 14:40:19 -0700

    added annotations to kitchencontroller

--- src/main/java/dev/J/RepositoryForFamilies/Kitchen/KitchenController.java
+++ src/main/java/dev/J/RepositoryForFamilies/Kitchen/KitchenController.java
@@ -1,7 +1,11 @@
 package dev.J.RepositoryForFamilies.Kitchen;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:4200"}, allowCredentials = "true")
 @RestController
 public class KitchenController {
 }
